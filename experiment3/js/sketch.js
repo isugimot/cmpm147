0a00b92aced7ef6cde5829d13c1d26a3b7f12eff
Ichiro Sugimoto <isugimot@ucsc.edu>
2024-04-21 12:41:07 -0700

    Testing things

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -28,80 +28,77 @@ $("#reseedButton").click(function() {
   seed++;
 });
 
-// setup() function is called once when the program starts
-function setup() {
-  // place our canvas, making it fit our container
-  canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-  canvas.parent("canvas-container");
-  $(window).resize(function() {
-    resizeScreen();
-  });
-  resizeScreen();
-}
-
-function gridToString(grid) {
-  let rows = [];
-  for (let i = 0; i < grid.length; i++) {
-    rows.push(grid[i].join(""));
-  }
-  return rows.join("\n");
-}
-
-function stringToGrid(str) {
-  let grid = [];
-  let lines = str.split("\n");
-  for (let i = 0; i < lines.length; i++) {
-    let row = [];
-    let chars = lines[i].split("");
-    for (let j = 0; j < chars.length; j++) {
-      row.push(chars[j]);
-    }
-    grid.push(row);
-  }
-  return grid;
-}
-
-function setup() {
-  numCols = select("#asciiBox").attribute("rows") | 0;
-  numRows = select("#asciiBox").attribute("cols") | 0;
-
-  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
-  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
-
-  select("#reseedButton").mousePressed(reseed);
-  select("#asciiBox").input(reparseGrid);
-
-  reseed();
-}
-
-
-function draw() {
-  randomSeed(seed);
-  drawGrid(currentGrid);
-}
-
-function placeTile(i, j, ti, tj) {
-  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
-}
-
 function generateGrid(numCols, numRows) {
-  //The grid with the different biomes and items
+  //Determines which side is smaller to pick the maximum
+  //and the minimum size of rooms
+  let smaller = 1;
+  if (numCols >= numRows){
+    smaller = numRows;
+  } else {
+    smaller = numCols;
+  }
+  //The size and number of rooms
+  let maxSize = floor(random(smaller/5, smaller/4));
+  let minSize = floor(random(smaller/10, maxSize));
+  let numRooms = floor(random(5, 10));
+  //The grid with the background areas
   let grid = [];
   for (let i = 0; i < numRows; i++) {
     let row = [];
     for (let j = 0; j < numCols; j++) {
       if(noise(i/10, j/10) > 0.5){
-        row.push("-");
-      }
-      else if(noise(i/10, j/10) < 0.3){
-        row.push("W");
+        row.push("-")
       }
       else{
         row.push("_");
       }
     }
     grid.push(row);
+  }
+  //This randomly locates and create rooms depending on the min and max size.
+  let rooms = [];
+  for (let i = 0; i < numRooms; i++){
+    const roomRows = floor(random() * (maxSize - minSize)) + minSize;
+    const roomCols = floor(random() * (maxSize - minSize)) + minSize;
+    const x = floor(random() * numCols - roomCols) + 1;
+    const y = floor(random() * numRows - roomRows) + 1;
+    const room = {x: x, y: y, width: roomCols, height: roomRows};
+    //This checks if the room is overlapping. If it is, the room is rebuilt before it is pushed to the sets of rooms
+    //to keep track the used spaces.
+    for(const r of rooms){
+      if(room.x < r.x + r.width && room.x + room.width > r.x && room.y < r.y + r.height && room.y + room.height > r.y){
+        i--;
+        continue;
+      }
+    }
+    rooms.push(room);
+    //This prints out the room into the grid.
+    for (let j = y; j < y + roomRows; j++){
+      for (let l = x; l < x + roomCols; l++){
+        grid[j][l] = ".";
+      }
+    }
+    
+    //This creates a passage between rooms, which creates an passage connecting the center of the room
+    //and the center of the room created previously
+    if (i > 0) {
+      const prevRoom = rooms[i - 1];
+      let prevCenterX = floor(prevRoom.x + prevRoom.width / 2);
+      let prevCenterY = floor(prevRoom.y + prevRoom.height / 2);
+      const centerX = floor(room.x + room.width / 2);
+      const centerY = floor(room.y + room.height / 2);
+      
+      while (prevCenterX !== centerX) {
+        grid[prevCenterY][prevCenterX] = ".";
+        if (prevCenterX < centerX) prevCenterX++;
+        else prevCenterX--;
+      }
+      while (prevCenterY !== centerY) {
+        grid[prevCenterY][prevCenterX] = ".";
+        if (prevCenterY < centerY) prevCenterY++;
+        else prevCenterY--;
+      }
+    }
   }
   return grid;
 }
@@ -186,63 +183,33 @@ function drawGrid(grid) {
   background(128);
   for(let i = 0; i < grid.length; i++) {
     for(let j = 0; j < grid[i].length; j++) {
-      //Drawing the water, andl also animating it using % operators and millis().
-      if(grid[i][j] == 'W'){
-        placeTile(i, j, 0, 13);
-        
-        placeTile(i, j, 0 + (millis()% (((i * j) % 17) * 5000) < (((i * j) % 5) + 5) * 50 ? 3 : 0), 13);
-      }
-      //Drawing the low lands
-      if(grid[i][j] == '-'){
-        placeTile(i, j, floor(random(0, 4)), 1);
-      }
-      else{
-        drawContext(grid, i, j, "-", 0, 6);
-      }
-      //Drawing the high lands with the houses and towers.
-      //Also drawing the trees in the low lands
-      //Drawing ground
+      //Drawing lavas with animations by millis()
       if (grid[i][j] == '_') {
-        let ti = floor(random(4))
-        let tj = 0
+        let ti = floor(random(9, 11))
+        let tj = floor(random(18, 19))
         placeTile(i, j, ti, tj);
-        //Drawing the houses
-        if(gridCode(grid, i, j, "_") == 15 && random() > 0.9){
-          placeTile(i, j, 26, 0);
-        }
+        placeTile(i, j, 9 + (millis()%3000 < 250 ? -1 : -2), 18 + (millis()%700 < 250 ? 1 : 2));
       }
-      else{
-        //Drawing the edges
-        drawContext(grid, i, j, "_", 5, 0)
-        //Drawing the trees
-        if(gridCode(grid, i, j, "_") == 0 && grid[i][j] != "W"){
-          let ti = 14
-          let tj = floor(random(3))
-          placeTile(i, j, ti, tj);
-        }
+      else if (grid[i][j] == '-') {
+        let ti = (floor(random(1, 3)))
+        let tj = floor(random(18, 19))
+        placeTile(i, j, ti, tj);
+        placeTile(i, j, 5 + (millis()%1000 > 800 ? 1 : 0), 19 + (millis()%2000 < 250 ? -1 : 0));
       }
-      //Drawing the towers
-      if(gridCode(grid, i, j, "_") == 15 && random() > 0.99){
-        placeTile(i, j, 28, 1);
-        placeTile(i - 1, j, 28, 0);
+      //Drawing the dungeon
+      if(gridCheck(grid, i, j, ".")){
+        placeTile(i, j, (floor(random(4))), 9);
+        //Adding extra treasure boxes as well.
+        if(random() < 0.01){
+          placeTile(i, j, 2, 30);
+        }
+      } else {
+        //Drawing the edges and also adding in some extra terrains in the lava.
+        drawContext(grid, i, j, ".", 0, 9);
+        if(gridCode(grid, i, j, ".") == 0 && random() > 0.99){
+          placeTile(i, j, 14, 9);
+        }
       }
     }
   }
-}
-
-function reseed() {
-  seed = (seed | 0) + 1109;
-  randomSeed(seed);
-  noiseSeed(seed);
-  select("#seedReport").html("seed " + seed);
-  regenerateGrid();
-}
-
-function regenerateGrid() {
-  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
-  reparseGrid();
-}
-
-function reparseGrid() {
-  currentGrid = stringToGrid(select("#asciiBox").value());
 }
