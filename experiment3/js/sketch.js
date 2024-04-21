876b2b45386def2a712920f4bfc2dabf67a43b90
Ichiro Sugimoto <isugimot@ucsc.edu>
2024-04-21 13:48:16 -0700

    This too

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -59,7 +59,7 @@ function setup() {
   numCols = select("#asciiBox").attribute("rows") | 0;
   numRows = select("#asciiBox").attribute("cols") | 0;
 
-  createCanvas(16 * numCols, 16 * numRows).parent("canvasContainer");
+  createCanvas(16 * numCols, 16 * numRows).parent("canvas-container");
   select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
 
   select("#reseedButton").mousePressed(reseed);
