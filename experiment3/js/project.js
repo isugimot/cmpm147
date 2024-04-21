1d1a577b65b1fe1aa0652efe29f1404709548f8a
Ichiro Sugimoto <isugimot@ucsc.edu>
2024-04-21 13:47:36 -0700

    Does this work

--- experiment3/js/project.js
+++ experiment3/js/project.js
@@ -2,75 +2,33 @@
 // Author: Your Name
 // Date:
 
-let seed = 0;
-let tilesetImage;
-let currentGrid = [];
-let numRows, numCols;
+// NOTE: This is how we might start a basic JavaaScript OOP project
 
-function preload() {
-  tilesetImage = loadImage(
-    "tilesetP8.png"
-  );
-}
+// Constants - User-servicable parts
+// In a longer project I like to put these in a separate file
 
-function reseed() {
-  seed = (seed | 0) + 1109;
-  randomSeed(seed);
-  noiseSeed(seed);
-  select("#seedReport").html("seed " + seed);
-  regenerateGrid();
-}
-
-function regenerateGrid() {
-  select("#asciiBox").value(gridToString(generateGrid(numCols, numRows)));
-  reparseGrid();
-}
-
-function reparseGrid() {
-  currentGrid = stringToGrid(select("#asciiBox").value());
-}
-
-function gridToString(grid) {
-  let rows = [];
-  for (let i = 0; i < grid.length; i++) {
-    rows.push(grid[i].join(""));
+// define a class
+class MyProjectClass {
+  // constructor function
+  constructor(param1, param2) {
+    // set properties using 'this' keyword
+    this.property1 = param1;
+    this.property2 = param2;
   }
-  return rows.join("\n");
-}
-
-function stringToGrid(str) {
-  let grid = [];
-  let lines = str.split("\n");
-  for (let i = 0; i < lines.length; i++) {
-    let row = [];
-    let chars = lines[i].split("");
-    for (let j = 0; j < chars.length; j++) {
-      row.push(chars[j]);
-    }
-    grid.push(row);
+  
+  // define a method
+  myMethod() {
+    // code to run when method is called
   }
-  return grid;
 }
 
-function setup() {
-  numCols = select("#asciiBox").attribute("rows") | 0;
-  numRows = select("#asciiBox").attribute("cols") | 0;
+function main() {
+  // create an instance of the class
+  let myInstance = new MyProjectClass("value1", "value2");
 
-  createCanvas(16 * numCols, 16 * numRows).parent("canvas-container");
-  select("canvas").elt.getContext("2d").imageSmoothingEnabled = false;
-
-  select("#reseedButton").mousePressed(reseed);
-  select("#asciiBox").input(reparseGrid);
-
-  reseed();
+  // call a method on the instance
+  myInstance.myMethod();
 }
 
-
-function draw() {
-  randomSeed(seed);
-  drawGrid(currentGrid);
-}
-
-function placeTile(i, j, ti, tj) {
-  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
-}
+// let's get this party started - uncomment me
+//main();
