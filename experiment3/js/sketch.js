72f8738e6dbcb5d15a75f52d4418e74053370055
Ichiro Sugimoto <isugimot@ucsc.edu>
2024-04-21 21:58:30 -0700

    Testing

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -19,7 +19,9 @@ function preload() {
   );
 }
 
-//Overworld generator
+function placeTile(i, j, ti, tj) {
+  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
+}
 
 //Dungeon generator
 
@@ -80,8 +82,197 @@ var myp5 = new p5((d) => {
     d.randomSeed(seed);
     drawGrid(currentGrid);
   }
+
+  function generateGrid(numCols, numRows) {
+    //Determines which side is smaller to pick the maximum
+    //and the minimum size of rooms
+    let smaller = 1;
+    if (numCols >= numRows){
+      smaller = numRows;
+    } else {
+      smaller = numCols;
+    }
+    //The size and number of rooms
+    let maxSize = d.floor(random(smaller/5, smaller/4));
+    let minSize = d.floor(random(smaller/10, maxSize));
+    let numRooms = d.floor(random(5, 10));
+    //The grid with the background areas
+    let grid = [];
+    for (let i = 0; i < numRows; i++) {
+      let row = [];
+      for (let j = 0; j < numCols; j++) {
+        if(d.noise(i/10, j/10) > 0.5){
+          row.push("-")
+        }
+        else{
+          row.push("_");
+        }
+      }
+      grid.push(row);
+    }
+    //This randomly locates and create rooms depending on the min and max size.
+    let rooms = [];
+    for (let i = 0; i < numRooms; i++){
+      const roomRows = d.floor(random() * (maxSize - minSize)) + minSize;
+      const roomCols = d.floor(random() * (maxSize - minSize)) + minSize;
+      const x = d.floor(random() * numCols - roomCols) + 1;
+      const y = d.floor(random() * numRows - roomRows) + 1;
+      const room = {x: x, y: y, width: roomCols, height: roomRows};
+      //This checks if the room is overlapping. If it is, the room is rebuilt before it is pushed to the sets of rooms
+      //to keep track the used spaces.
+      for(const r of rooms){
+        if(room.x < r.x + r.width && room.x + room.width > r.x && room.y < r.y + r.height && room.y + room.height > r.y){
+          i--;
+          continue;
+        }
+      }
+      rooms.push(room);
+      //This prints out the room into the grid.
+      for (let j = y; j < y + roomRows; j++){
+        for (let l = x; l < x + roomCols; l++){
+          grid[j][l] = ".";
+        }
+      }
+      
+      //This creates a passage between rooms, which creates an passage connecting the center of the room
+      //and the center of the room created previously
+      if (i > 0) {
+        const prevRoom = rooms[i - 1];
+        let prevCenterX = d.floor(prevRoom.x + prevRoom.width / 2);
+        let prevCenterY = d.floor(prevRoom.y + prevRoom.height / 2);
+        const centerX = d.floor(room.x + room.width / 2);
+        const centerY = d.floor(room.y + room.height / 2);
+        
+        while (prevCenterX !== centerX) {
+          grid[prevCenterY][prevCenterX] = ".";
+          if (prevCenterX < centerX) prevCenterX++;
+          else prevCenterX--;
+        }
+        while (prevCenterY !== centerY) {
+          grid[prevCenterY][prevCenterX] = ".";
+          if (prevCenterY < centerY) prevCenterY++;
+          else prevCenterY--;
+        }
+      }
+    }
+    return grid;
+  }
+  
+  // [1, 21] is dungeon wall
+  // [0, 9] is dungeon floor
+  const lookup = [
+    [5, 1], //0000 blank
+    [5, 0], //0001 up
+    [6, 1], //0010 right
+    [6, 0], //0011 up right
+    [5, 2], //0100 down
+    [5, 1], //0101 down & up 
+    [6, 2], //0110 down right
+    [5, 1], //0111 down, up, and right
+    [4, 1], //1000 left
+    [4, 0], //1001 up left
+    [5, 1], //1010 left right 10
+    [5, 1], //1011 left right up
+    [4, 2], //1100 down left
+    [5, 1], //1101 down left up
+    [5, 1], //1110 down left right
+    [5, 1] //1111 all direction
+  ];
+  
+  //Check the coordination if it is in the area and if it is the target
+  function gridCheck(grid, i, j, target) {
+    if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length){
+      return false;
+    }
+    if(grid[i][j] == target){
+      return true;
+    }
+    return false;
+  }
+  
+  //Adds in to a string depending on if the target object is located around the coordinate.
+  //The string(binary) is converted into decimal and returned
+  function gridCode(grid, i, j, target) {
+    let binary = '';
+    binary += gridCheck(grid, i, j - 1, target) ? "1":"0";
+    binary += gridCheck(grid, i + 1, j, target)  ? "1":"0";
+    binary += gridCheck(grid, i, j + 1, target)  ? "1":"0";
+    binary += gridCheck(grid, i - 1, j, target)  ? "1":"0";
+    const index = parseInt(binary, 2);
+    return index
+  }
+  
+  //Gets the index number from gridCode and place tiles depending what the value is.
+  function drawContext(grid, i, j, target, dti, dtj) {
+    const code = gridCode(grid, i, j, target)
+    if(code == 5){
+      placeTile(i, j, dti + 5, dtj + 2);
+      placeTile(i, j, dti + 5, dtj);
+    } else if(code == 7){
+      placeTile(i, j, dti + 5, dtj + 2);
+      placeTile(i, j, dti + 6, dtj);
+    } else if (code == 10){
+      placeTile(i, j, dti + 4, dtj + 1);
+      placeTile(i, j, dti + 6, dtj + 1);
+    } else if (code == 11){
+      placeTile(i, j, dti + 4, dtj + 1);
+      placeTile(i, j, dti + 6, dtj);
+    } else if (code == 13){
+      placeTile(i, j, dti + 4, dtj + 2);
+      placeTile(i, j, dti + 5, dtj);
+    } else if (code == 14){
+      placeTile(i, j, dti + 4, dtj + 2);
+      placeTile(i, j, dti + 6, dtj + 1);
+    } else if (code == 15){
+      placeTile(i, j, dti + 4, dtj + 2);
+      placeTile(i, j, dti + 6, dtj);
+    }
+    else{
+      const [tiOffset, tjOffset] = lookup[code];
+      placeTile(i, j, dti + tiOffset, dtj + tjOffset);
+    }
+  }
+  
+  //Drawing grids.
+  function drawGrid(grid) {
+    background(128);
+    for(let i = 0; i < grid.length; i++) {
+      for(let j = 0; j < grid[i].length; j++) {
+        //Drawing lavas with animations by millis()
+        if (grid[i][j] == '_') {
+          let ti = d.floor(random(9, 11))
+          let tj = d.floor(random(18, 19))
+          placeTile(i, j, ti, tj);
+          placeTile(i, j, 9 + (millis()%3000 < 250 ? -1 : -2), 18 + (millis()%700 < 250 ? 1 : 2));
+        }
+        else if (grid[i][j] == '-') {
+          let ti = (d.floor(random(1, 3)))
+          let tj = d.floor(random(18, 19))
+          placeTile(i, j, ti, tj);
+          placeTile(i, j, 5 + (millis()%1000 > 800 ? 1 : 0), 19 + (millis()%2000 < 250 ? -1 : 0));
+        }
+        //Drawing the dungeon
+        if(gridCheck(grid, i, j, ".")){
+          placeTile(i, j, (d.floor(random(4))), 9);
+          //Adding extra treasure boxes as well.
+          if(random() < 0.01){
+            placeTile(i, j, 2, 30);
+          }
+        } else {
+          //Drawing the edges and also adding in some extra terrains in the lava.
+          drawContext(grid, i, j, ".", 0, 9);
+          if(gridCode(grid, i, j, ".") == 0 && random() > 0.99){
+            placeTile(i, j, 14, 9);
+          }
+        }
+      }
+    }
+  }
+
 }, 'p5sketch');
 
+
+//Overworld generator
 var myp5 = new p5((o) => {
   function reseed2() {
     seed2 = (seed2 | 0) + 1109;
@@ -139,6 +330,152 @@ var myp5 = new p5((o) => {
     o.randomSeed(seed2);
     drawGrid2(currentGrid2);
   }
+
+  function generateWorldGrid(numCols, numRows) {
+    //The grid with the different biomes and items
+    let grid = [];
+    for (let i = 0; i < numRows; i++) {
+      let row = [];
+      for (let j = 0; j < numCols; j++) {
+        if(noise(i/10, j/10) > 0.5){
+          row.push("-");
+        }
+        else if(noise(i/10, j/10) < 0.3){
+          row.push("W");
+        }
+        else{
+          row.push("_");
+        }
+      }
+      grid.push(row);
+    }
+    return grid;
+  }
+  
+  // [1, 21] is dungeon wall
+  // [0, 9] is dungeon floor
+  const lookup = [
+    [5, 1], //0000 blank
+    [5, 0], //0001 up
+    [6, 1], //0010 right
+    [6, 0], //0011 up right
+    [5, 2], //0100 down
+    [5, 1], //0101 down & up 
+    [6, 2], //0110 down right
+    [5, 1], //0111 down, up, and right
+    [4, 1], //1000 left
+    [4, 0], //1001 up left
+    [5, 1], //1010 left right 10
+    [5, 1], //1011 left right up
+    [4, 2], //1100 down left
+    [5, 1], //1101 down left up
+    [5, 1], //1110 down left right
+    [5, 1] //1111 all direction
+  ];
+  
+  //Check the coordination if it is in the area and if it is the target
+  function gridCheck(grid, i, j, target) {
+    if(i < 0 || j < 0 || i >= grid.length || j >= grid[i].length){
+      return false;
+    }
+    if(grid[i][j] == target){
+      return true;
+    }
+    return false;
+  }
+  
+  //Adds in to a string depending on if the target object is located around the coordinate.
+  //The string(binary) is converted into decimal and returned
+  function gridCode(grid, i, j, target) {
+    let binary = '';
+    binary += gridCheck(grid, i, j - 1, target) ? "1":"0";
+    binary += gridCheck(grid, i + 1, j, target)  ? "1":"0";
+    binary += gridCheck(grid, i, j + 1, target)  ? "1":"0";
+    binary += gridCheck(grid, i - 1, j, target)  ? "1":"0";
+    const index = parseInt(binary, 2);
+    return index
+  }
+  
+  //Gets the index number from gridCode and place tiles depending what the value is.
+  function drawContext(grid, i, j, target, dti, dtj) {
+    const code = gridCode(grid, i, j, target)
+    if(code == 5){
+      placeTile(i, j, dti + 5, dtj + 2);
+      placeTile(i, j, dti + 5, dtj);
+    } else if(code == 7){
+      placeTile(i, j, dti + 5, dtj + 2);
+      placeTile(i, j, dti + 6, dtj);
+    } else if (code == 10){
+      placeTile(i, j, dti + 4, dtj + 1);
+      placeTile(i, j, dti + 6, dtj + 1);
+    } else if (code == 11){
+      placeTile(i, j, dti + 4, dtj + 1);
+      placeTile(i, j, dti + 6, dtj);
+    } else if (code == 13){
+      placeTile(i, j, dti + 4, dtj + 2);
+      placeTile(i, j, dti + 5, dtj);
+    } else if (code == 14){
+      placeTile(i, j, dti + 4, dtj + 2);
+      placeTile(i, j, dti + 6, dtj + 1);
+    } else if (code == 15){
+      placeTile(i, j, dti + 4, dtj + 2);
+      placeTile(i, j, dti + 6, dtj);
+    }
+    else{
+      const [tiOffset, tjOffset] = lookup[code];
+      placeTile(i, j, dti + tiOffset, dtj + tjOffset);
+    }
+  }
+  
+  //Drawing grids.
+  function drawWorldGrid(grid) {
+    background(128);
+    for(let i = 0; i < grid.length; i++) {
+      for(let j = 0; j < grid[i].length; j++) {
+        //Drawing the water, andl also animating it using % operators and millis().
+        if(grid[i][j] == 'W'){
+          placeTile(i, j, 0, 13);
+        
+          placeTile(i, j, 0 + (millis()% (((i * j) % 17) * 5000) < (((i * j) % 5) + 5) * 50 ? 3 : 0), 13);
+        }
+        //Drawing the low lands
+        if(grid[i][j] == '-'){
+          placeTile(i, j, floor(random(0, 4)), 1);
+        }
+        else{
+          drawContext(grid, i, j, "-", 0, 6);
+        }
+        //Drawing the high lands with the houses and towers.
+        //Also drawing the trees in the low lands
+        //Drawing ground
+        if (grid[i][j] == '_') {
+          let ti = floor(random(4))
+          let tj = 0
+          placeTile(i, j, ti, tj);
+          //Drawing the houses
+          if(gridCode(grid, i, j, "_") == 15 && random() > 0.9){
+            placeTile(i, j, 26, 0);
+          }
+        }
+        else{
+          //Drawing the edges
+          drawContext(grid, i, j, "_", 5, 0)
+          //Drawing the trees
+          if(gridCode(grid, i, j, "_") == 0 && grid[i][j] != "W"){
+            let ti = 14
+            let tj = floor(random(3))
+            placeTile(i, j, ti, tj);
+          }
+        }
+        //Drawing the towers
+        if(gridCode(grid, i, j, "_") == 15 && random() > 0.99){
+          placeTile(i, j, 28, 1);
+          placeTile(i - 1, j, 28, 0);
+        }
+      }
+    }
+  }
+
 }, 'p5sketch');
 
 /*function setup() {
@@ -169,10 +506,6 @@ var myp5 = new p5((o) => {
 function draw() {
   randomSeed(seed2);
   drawWorldGrid(currentGrid2);
-}*/
-
-function placeTile(i, j, ti, tj) {
-  image(tilesetImage, 16 * j, 16 * i, 16, 16, 8 * ti, 8 * tj, 8, 8);
 }
 
 function generateGrid(numCols, numRows) {
@@ -429,4 +762,4 @@ function drawWorldGrid(grid) {
       }
     }
   }
-}
+}*/
